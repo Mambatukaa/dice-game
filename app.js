9e77ca7297bfd73bad0e2522a8f9c637e317d72e
batuka <sbatuka9@gmail.com>
2020-06-09 16:30:09 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -12,22 +12,21 @@ var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүй үүсгэж өгнө
 
-var dice = Math.floor(Math.random() * 6) + 1;
-
-// <div class="player-score" id="score-0">43</div>
-
-// document.querySelector("#score-0").textContent = dice;
-
-// document.querySelector("#score-1").innerHTML = dice;
-
 // Програм эхлэхэд бэлтгэх
 
-document.querySelector("#score-0").textContent = "0";
-document.querySelector("#score-1").textContent = "0";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = "0";
-document.querySelector("#current-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-console.log("Шоо :" + dice);
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
